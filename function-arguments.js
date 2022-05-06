27be78e695d46df9dbc4ecfc20f6f9684ecfe3a4
pixiesprye <99289898+pixiesprye@users.noreply.github.com>
2022-05-06 16:49:47 -0400

    Add files via upload

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function math (Rosara, Cassin, Poeisis) {
+    return (Rosara + (Cassin * Poeisis));
+}
+console.log(math(53, 61, 67));
---
+++ string-length.js
@@ -0,0 +1,3 @@
+let example = 'example string'
+example.length
+console.log(14)
